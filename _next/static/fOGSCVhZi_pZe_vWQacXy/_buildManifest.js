63b6135ae22a050c49723b92321ba8c76b4ea00a
bppumm <bppumm1212@gmail.com>
2023-03-31 00:39:23 +0800

    Updates

---
+++ _next/static/fOGSCVhZi_pZe_vWQacXy/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/d01c6cdb137b10ce.css","static/chunks/pages/index-d007f427b952967f.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
